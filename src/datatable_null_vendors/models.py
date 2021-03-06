5fa966bb033d8898b64c3d9555bb293347eba91e
Work <davidblancoferrandez@gmail.com>
2021-03-06 17:56:41 +0100

    avance

--- src/datatable_null_vendors/models.py
+++ src/datatable_null_vendors/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class DataTable(models.Model):
-    vendorid = models.FloatField()
+    id = models.IntegerField(primary_key=True)
+    vendorid = models.FloatField(null=True)
     tpep_pickup_datetime = models.DateTimeField()
     trip_distance = models.FloatField()
-    payment_type = models.FloatField()
+    payment_type = models.FloatField(null=True)
--- src/pages/views.py
+++ src/pages/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
+from datatable_null_vendors.models import DataTable
 # Create your views here.
 
 
@@ -10,4 +12,19 @@ def home_view(*args, **kwargs):
 
 
 def panel(request, *args, **kwargs):
-    return render(request, "base.html", {})
+    rows = DataTable.objects.all()
+    paginator = Paginator(rows, 100)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, "base.html", {'page_obj': page_obj})
+
+'''
+   context = {
+        'id': obj.id,
+        'vendorid': obj.vendorid,
+        'tpep_pickup_datetime': obj.tpep_pickup_datetime,
+        'trip_distance': obj.trip_distance,
+        'payment_type': obj.payment_type
+    }
+'''
