372f3eeb272e3b5742833e0b89eb71452dc2f04c
Work <davidblancoferrandez@gmail.com>
2021-03-08 17:11:46 +0100

    avance

--- src/pages/views.py
+++ src/pages/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.core.paginator import Paginator
 from django.db.models import F
+from rest_framework import generics
 
 from datatable_null_vendors.models import DataTable
 from rest_framework import viewsets
@@ -24,21 +25,6 @@ def panel(request, *args, **kwargs):
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
-    '''
-    data = DataTable.objects.all()
-    paginator = Paginator(data, 100)
-    page = request.GET.get('page', 1)
-
-    try:
-        page_obj = paginator.page(page)
-    except PageNotAnInteger:
-        page_obj = paginator.page(1)
-    except EmptyPage:
-        page_obj = paginator.page(paginator.num_pages)
-    
-    page_obj = DataTable.objects.all()
-    '''
-
     return render(request, "base.html", {'page_obj': page_obj})
 
 
@@ -47,8 +33,19 @@ def edit(request, id):
     return render(request, 'edit.html', {'row': row})
 
 
-class TripViewSet(viewsets.ModelViewSet):
-    queryset = Yellowtaxis.objects.all().order_by('vendorid'
-        #F('trip_distance').desc(nulls_last=True)
-    )
+class TripViewSet(
+    #generics.ListAPIView
+    viewsets.ReadOnlyModelViewSet
+):
+    queryset = Yellowtaxis.objects.all().order_by('vendorid')
     serializer_class = TripSerializer
+
+'''    def get_queryset(self):
+        queryset = Yellowtaxis.objects.all()
+        vendor = self.request.query_params.get('vendorid', None)
+        if vendor is not None:
+            queryset = queryset.filter(vendorid__username=vendor)
+        return queryset
+'''
+
+#F('trip_distance').desc(nulls_last=True)
